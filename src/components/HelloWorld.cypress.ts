d72dc1175f8b6d0f38ebdeb003674c4d7259f10e
kayak <kayak@kayak.rocks>
2021-10-06 22:21:31 +1000

    🎨 (prettier): add config; format files

--- src/components/HelloWorld.cypress.ts
+++ src/components/HelloWorld.cypress.ts
@@ -1,22 +1,22 @@
-import { mount } from "@cypress/vue";
-import HelloWorld from "./HelloWorld.vue";
+import { mount } from '@cypress/vue'
+import HelloWorld from './HelloWorld.vue'
 
 beforeEach(() => {
   mount(HelloWorld, {
     propsData: {
-      msg: "Hello World",
+      msg: 'Hello World',
     },
-  });
-});
+  })
+})
 
-it("shows the header", () => {
-  cy.get("h1").contains("Hello World");
-});
+it('shows the header', () => {
+  cy.get('h1').contains('Hello World')
+})
 
-it("renders the counter", () => {
-  cy.get("button").contains("count is: 0");
-});
+it('renders the counter', () => {
+  cy.get('button').contains('count is: 0')
+})
 
-it("increments counter when clicked", () => {
-  cy.get("button").click().contains("count is: 1");
-});
+it('increments counter when clicked', () => {
+  cy.get('button').click().contains('count is: 1')
+})
