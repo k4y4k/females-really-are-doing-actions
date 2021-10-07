c9f02e274a5e703e108d7c57e3d4eea7e0d5f9aa
kayak <kayak@kayak.rocks>
2021-10-07 16:06:47 +1000

    ➖ (windi): windi doesn't work

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,15 +1,7 @@
-const WindiCSS = require('windicss')
-
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
   core: {
     builder: 'storybook-builder-vite',
-  },
-
-  async viteFinal(config, { configType }) {
-    config.plugins = config.plugins ?? []
-    config.plugins.push(WindiCSS())
-    return config
   },
 }
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,3 @@
-import 'virtual:windi.css'
-
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
