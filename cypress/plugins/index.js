e08a3a37bc6f1303be935d267d85ccea8a932a75
kayak <kayak@kayak.rocks>
2021-10-08 14:52:10 +1000

    ⚗️ (chrome): forbid chrome from using /dev/shm

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -13,5 +13,13 @@ module.exports = (on, config) => {
     })
   })
 
+  on('before:browser:launch', (browser, launchOptions) => {
+    if (browser.name === 'chrome') {
+      launchOptions.args.push('--disable-dev-shm-usage')
+    }
+
+    return launchOptions
+  })
+
   return config
 }
